04929987abb5f733a9f606a73b4a2b54b0ca740d
luiggy_mamani <luiggy.mamani.12777190@usalesiana.edu.bo>
2023-03-06 16:48:44 -0400

    build: :wrench: add cors dependency

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import express, { urlencoded } from 'express';
+import cors from 'cors';
 
 import indexRoutes from './routes/index.routes.js';
 import productRoutes from './routes/products.routes.js';
@@ -8,6 +9,7 @@ const app = express();
 
 // config
 app.set('port', process.env.PORT || 4000);
+app.use(cors());
 app.use(urlencoded({ extended: false }));
 
 // routes
